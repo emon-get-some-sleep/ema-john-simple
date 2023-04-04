7ab7d79bc0953f9ae959c386d7e9b7d48d263371
emonh0que <you@example.com>
2023-04-05 01:05:06 +0600

    added review item

--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './ReviewItem.css';
+import DeleteBin2FillIcon from 'remixicon-react/DeleteBin2FillIcon';
 const ReviewItem = ({product}) => {
     const {id, img, name , price , quantity} = product;
     return (
         <div className='review-item'>
             <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+                <DeleteBin2FillIcon className='delete-icon' />
+            </button>
         </div>
     );
 };
