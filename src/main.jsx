0bc9bc75ee02c25aad4cb401e08867feb43b7e0d
emonh0que <you@example.com>
2023-04-04 21:21:15 +0600

    added routes for login, inventory, order

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './component/Shop/Shop';
+import Home from './component/Layout/Home';
+import Order from './component/Order/Order';
+import Inventory from './component/Inventory/Inventory';
+import Login from './component/Login/Login';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Order></Order>
+    
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
+  },
+  
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
