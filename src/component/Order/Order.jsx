736f60ecc04527e650ce0568d68ce23ee589a7fd
emonh0que <you@example.com>
2023-04-05 10:46:34 +0600

    implemented delete from cart

--- src/component/Order/Order.jsx
+++ src/component/Order/Order.jsx
@@ -3,6 +3,7 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Order.css';
+import { removeFromDb } from '../../utilities/fakedb';
 const Order = () => {
     const savedCart = useLoaderData();
     const [cart, setCart] = useState(savedCart)
@@ -11,6 +12,7 @@ const Order = () => {
     const handleRemoveFromCart = (id) => {
         const remaining = cart.filter(product => product.id !== id);
         setCart(remaining);
+        removeFromDb(id);
     }
     return (
         <div className='shop-container'>
